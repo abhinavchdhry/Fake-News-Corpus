61bfeaef153900426417b36c86864c69e9a5c65e
Abhinav <abhinav155.bits@gmail.com>
2017-01-31 03:37:06 -0500

    Restructuring

--- src/data/scripts/extract_remaining.py
+++ src/data/scripts/extract_remaining.py
@@ -14,15 +14,15 @@ import urllib2
 import hashlib
 import requests
 
-f = open("combined.data", "r")
+f = open("../combined.data", "r")
 all_urls = []
 for line in f:
 	all_urls.append(str(line.strip()))
 
 
 done = []
-for fi in os.listdir("./content/"):
-	f1 = open("./content/" + fi, "r")
+for fi in os.listdir("../content/"):
+	f1 = open("../content/" + fi, "r")
 	for line in f1:
 		d = json.loads(line)
 		url = d["url"]
@@ -37,7 +37,6 @@ count = 0
 for url in remaining:
 	try:
 		print(str(count+1) + ": Current URL: " + url)
-#		content = urllib2.urlopen(url, timeout=3).read()
 		content = requests.get(url).text.encode('ascii', 'ignore')
 		d= {}
 		d["url"] = url
@@ -45,7 +44,7 @@ for url in remaining:
 		m = hashlib.md5()
 		m.update(url)
 		filename = m.hexdigest()
-		f2 = open("content/" + filename + ".txt", "w+")
+		f2 = open("../content/" + filename + ".txt", "w+")
 		json.dump(d, f2)
 		f2.close()
 		count = count + 1
